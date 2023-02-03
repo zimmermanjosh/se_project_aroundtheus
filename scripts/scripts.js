089ca335380c2565a2a6d33b6941496c22e37065
Joshua Zimmerman <Joshua.Zimmerman@lexialearning.com>
2023-02-03 17:50:53 -0600

    feature copy and update add/image modals /n  update: added scripts for html code

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,7 +5,7 @@ this is my re-org to  help me apply work and school, this need to be the same
 array of pics for app
 **/
 // these are not going to be reassigned in the future remove let
-const initialCards = [
+const initializeElement = [
   {
     //object 1
     name: "Yosemite Valley",
@@ -45,6 +45,7 @@ elements hooks for app
 =========================================================
 const
 **/
+/**  profile **/
 const profileEditButton = document.querySelector("#profile-edit-button");
 const profileEditModal = document.querySelector("#profile-edit-modal");
 const profileCloseButton = document.querySelector("#modal-close-button");
@@ -53,6 +54,16 @@ const profileDescription = document.querySelector("#profile-descr");
 const profileTitleInput = document.querySelector("#profile-title-input");
 const profileDescriptionInput = document.querySelector("#profile-descr-input");
 const profileEditForm = profileEditModal.querySelector("#profile-edit-form");
+
+/**  element **/
+const elementAddModal = document.querySelector("#element-add-modal");
+const elementAddButton = document.querySelector("#profile-add-button");
+const elementTitle = document.querySelector("#element-title");
+const elementImage = document.querySelector("#element-image");
+const elementTitleInput = document.querySelector("#element-title-input");
+const elementImageInput = document.querySelector("#element-image-input");
+const elementCloseButton = elementAddModal.querySelector("#element-add-close");
+
 /**
 =========================================================
 javascript for app
@@ -64,17 +75,30 @@ functions
 function closePopUp(popUp) {
   popUp.classList.remove("modal_opened");
 }
-
 function openPopUp(popUp) {
   popUp.classList.add("modal_opened");
 }
-
 function handleProfileEditSubmit(e) {
   e.preventDefault();
   profileTitle.textContent = profileTitleInput.value;
   profileDescription.textContent = profileDescriptionInput.value;
   closePopUp(profileEditModal);
 }
+
+function getCardView(elementData) {
+  const elementElement = elementTemplate.cloneNode(true);
+  const elementImageElement = elementElement.querySelector(".element__image");
+  const elementTitleElement = elementElement.querySelector(".element__title");
+  const elementDeleteButton = elementElement.querySelector(
+    "#element-delete-button"
+  );
+
+  cardImageElement.src = cardData.link;
+  cardImageElement.alt = cardData.name;
+  cardTitleElement.textContent = cardData.name;
+  return cardElement;
+}
+
 /**
 =========================================================
 javascript for app
@@ -83,17 +107,26 @@ elements hooks for app
 =========================================================
 event listeners
 **/
-
 profileEditButton.addEventListener("click", () => {
   profileTitleInput.value = profileTitle.textContent;
   profileDescriptionInput.value = profileDescription.textContent;
   openPopUp(profileEditModal);
 });
-
 profileCloseButton.addEventListener("click", () => {
   closePopUp(profileEditModal);
 });
 profileEditForm.addEventListener("submit", handleProfileEditSubmit);
+
+elementCloseButton.addEventListener("click", () => {
+  closePopUp(profileEditModal);
+});
+profileEditForm.addEventListener("submit", handleProfileEditSubmit);
+
+initializeElement.forEach((elementData) => {
+  const elementView = getCardView(elementData);
+  renderCard(elementView, elementListElement);
+});
+
 /**
 =========================================================
 javascript for app
