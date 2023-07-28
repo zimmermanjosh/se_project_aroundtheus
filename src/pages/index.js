630a51a7f5d31861764215a4646a8fde4b9ea164
Joshua Zimmerman <Joshua.Zimmerman@lexialearning.com>
2023-07-27 19:07:17 -0500

    updated locations

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,32 @@
-// ... (previous code)
+import Card from '../components/card.js';
+import FormValidator from '../components/./formValidator.js';
+import { openModal, closeModal, handlePopupClose } from "../utils/utils.js";
+import Section from '../utils/section.js';
+import UserInfo from '../utils/userInfo.js';
+import {
+  initializeCards,
+  profileEditModal,
+  profileEditButton,
+  profileTitleInput,
+  profileDescriptionInput,
+  profileTitle,
+  profileDescription,
+  profileEditForm,
+  elementAddModal,
+  elementAddButton,
+  elementAddForm,
+  elNameInput,
+  elUrlInput,
+  elementImageModal,
+  elementGallery,
+} from '/src/constants/variables'
 
 // Create an instance of the UserInfo class
 const userInfo = new UserInfo({
   nameSelector: '.profile__title',
   jobSelector: '.profile__descr',
 });
+
 
 // object configuration
 const config = {
@@ -22,19 +44,12 @@ editProfileValidator.enableValidation();
 const editImageValidator = new FormValidator(config, elementAddModal)
 editImageValidator.enableValidation();
 
-const profileEditPopup = new Popup(profileEditModal);
-
 
 // Functions
-/*function renderElement(elementData) {
-  const card = new Card(elementData, "#element-template", elementImageModal);
-  const cardElement = card.generateCard();
-  elementGallery.prepend(cardElement);
-}*/
 function renderElement(elementData) {
   const card = new Card(elementData, "#element-template", elementImageModal);
   const cardElement = card.generateCard();
-  elementSection.addItem(cardElement); // Use the addItem method of the Section class
+  elementGallery.prepend(cardElement);
 }
 
 // Create an instance of the Section class
@@ -43,18 +58,37 @@ const elementSection = new Section({ items: initializeCards, renderer: renderEle
 // Render the items on the page
 elementSection.renderItems();
 
-function handleProfileEditSubmit(name, job) {
+function handleProfileEditSubmit(evt) {
+  evt.preventDefault();
+
+  // Get the form values
+  const name = profileTitleInput.value;
+  const job = profileDescriptionInput.value;
+
   // Update the user info on the page
   userInfo.setUserInfo({ name, job });
 
-  //closeModal(profileEditModal);
-  profileEditPopup.close();
-
+  closeModal(profileEditModal);
   editProfileValidator.resetValidation(); // Reset validation for the profile edit form
 }
 
+// Function to handle form submission for adding new elements
 function handleElementImageModal(evt) {
-  // ... (previous code)
+  evt.preventDefault();
+  const name = elNameInput.value;
+  const url = elUrlInput.value;
+
+  const elementData = {
+    name: name,
+    url: url,
+  };
+
+  renderElement(elementData);
+
+  closeModal(elementAddModal);
+
+  // Reset the form validation for the element add form
+  editImageValidator.resetValidation();
 }
 
 /* event Listeners */
@@ -73,21 +107,8 @@ elementAddButton.addEventListener("click", () => {
 profileEditButton.addEventListener("click", () => {
   profileTitleInput.value = profileTitle.textContent;
   profileDescriptionInput.value = profileDescription.textContent;
-
-  profileEditPopup.open();
-  //openModal(profileEditModal);
+  openModal(profileEditModal);
 });
 
-// Event listener for profile edit form submission
-profileEditForm.addEventListener("submit", (evt) => {
-  evt.preventDefault();
-
-  // Get the form values
-  const name = profileTitleInput.value;
-  const job = profileDescriptionInput.value;
-
-  // Call the handleProfileEditSubmit function with the form values
-  handleProfileEditSubmit(name, job);
-});
-
+profileEditForm.addEventListener("submit", handleProfileEditSubmit);
 elementAddForm.addEventListener("submit", handleElementImageModal);
--- src/utils/popup.js
+++ src/utils/popup.js
@@ -1,35 +1,64 @@
-export default class Popup {
-  constructor(popupSelector) {
-    this._popup = document.querySelector(popupSelector);
-    this._closeButton = this._popup.querySelector('.modal__close');
-    this._handleEscClose = this._handleEscClose.bind(this);
+// Import the PopupWithImage class
+import PopupWithImage from './PopupwithForm.js';
+
+class Card {
+  constructor(data, templateSelector, elementImageModal) {
+    this._data = data;
+    this._templateSelector = templateSelector;
+    this._elementImageModal = elementImageModal;
   }
 
-  open() {
-    this._popup.classList.add('open');
-    document.addEventListener('keydown', this._handleEscClose);
+  _getTemplate() {
+    const cardTemplate = document.querySelector(this._templateSelector);
+    return cardTemplate.content.cloneNode(true);
   }
 
-  close() {
-    this._popup.classList.remove('open');
-    document.removeEventListener('keydown', this._handleEscClose);
+  _setEventListeners() {
+    this._cardElement.querySelector('.element__like-button').addEventListener('click', this._handleLikeButton.bind(this));
+    this._cardElement.querySelector('.element__delete-button').addEventListener('click', this._handleDeleteButton.bind(this));
+    this._cardElement.querySelector('.element__img').addEventListener('click', this._handleImageClick.bind(this));
   }
 
-  _handleEscClose(event) {
-    if (event.key === 'Escape') {
-      this.close();
-    }
+  _handleLikeButton(evt) {
+    const cardLike = evt.currentTarget;
+    cardLike.classList.toggle('element__like-button_active');
   }
 
-  setEventListeners() {
-    this._closeButton.addEventListener('click', () => {
-      this.close();
-    });
+  _handleDeleteButton(evt) {
+    const cardElement = evt.target.closest('.element');
+    cardElement.remove();
+  }
 
-    this._popup.addEventListener('mousedown', (event) => {
-      if (event.target === this._popup) {
-        this.close();
-      }
-    });
+  _handleImageClick(evt) {
+    const imageUrl = this._data.url;
+    const imageCaption = this._data.name;
+
+    // Create an instance of the PopupWithImage class and pass the imageUrl and imageCaption
+    const popupWithImage = new PopupWithImage(imageUrl, imageCaption);
+    popupWithImage.open(); // Open the popup with the image and caption
+
+    // Alternatively, you can use the existing elementImageModal and openModal function:
+    // const modalImage = this._elementImageModal.querySelector('#element-modal-image');
+    // const modalCaption = this._elementImageModal.querySelector('#element-modal-caption');
+    // modalImage.src = imageUrl;
+    // modalImage.alt = imageCaption;
+    // modalCaption.textContent = imageCaption;
+    // openModal(this._elementImageModal);
+  }
+
+  generateCard() {
+    this._cardElement = this._getTemplate().querySelector('.element');
+    const imageElement = this._cardElement.querySelector('.element__img');
+    const titleElement = this._cardElement.querySelector('.element__text');
+
+    imageElement.src = this._data.url;
+    imageElement.alt = this._data.name;
+    titleElement.textContent = this._data.name;
+
+    this._setEventListeners();
+
+    return this._cardElement;
   }
 }
+
+export default Card;
