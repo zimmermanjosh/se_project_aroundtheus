8ff1b02a47469fa7f6ff759284ad53398aab8a74
Joshua Zimmerman <Joshua.Zimmerman@lexialearning.com>
2023-08-13 15:36:44 -0500

    update: popupWithImage.js class

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -1,13 +1,19 @@
 import Popup from "./Popup.js";
-import {initializeCards} from "/src/constants/variables.js"
+import { initializeCards } from "/src/constants/variables.js";
+
 export default class PopupWithImage extends Popup {
   constructor(popupSelector) {
     super(popupSelector);
 
-    this._modalImage = this._modalImage.querySelector(".element__img");
-    this._modalCaption = this._modalCaption.querySelector(".element__descr");
-   }
-   open(){
+    this._modalImage = this._modalElement.querySelector(".element__img");
+    this._modalCaption = this._modalElement.querySelector(".element__descr");
+  }
+
+  open(imageUrl, imageCaption) {
+    this._modalImage.src = imageUrl;
+    this._modalImage.alt = imageCaption;
+    this._modalCaption.textContent = imageCaption;
+
     super.open();
-  };
-};
+  }
+}
